304974ba69a31957dd46e816889129545e232b23
James Henager <jameschenager@gmail.com>
2020-10-02 15:44:33 -0700

    add worklog update

---
+++ src/components/1-App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "./Header";
+import GameControl from "./2-GameControl";
+
+
+function App() {
+  return (
+    <React.Fragment>
+      <Header />
+      <GameControl />
+    </React.Fragment>
+  );
+}
+
+export default App;
