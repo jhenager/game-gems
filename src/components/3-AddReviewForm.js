2e817a83abcce38ee853dff67a474dfddfe57dda
James Henager <jameschenager@gmail.com>
2020-10-06 14:58:59 -0700

    add working reviewform that updates properties with votes from dropdowns

--- src/components/3-AddReviewForm.js
+++ src/components/3-AddReviewForm.js
@@ -13,22 +13,77 @@ function AddReviewForm(props) {
     event.preventDefault()
     props.onAddReview();
     let gameplayValue = 0;
+    let storyValue = 0;
+    let styleValue = 0;
+    let challengeValue = 0;
+    let replayValue = 0;
+    let socialValue = 0;
+    let innovationValue = 0;
+    let freedomValue = 0;
 
-    if (event.target.drop1.value === "gameplay") {
+    if (event.target.drop1.value  === "gameplay") {
       gameplayValue +=1;
+    }  else if (event.target.drop1.value === "story"){
+      storyValue+=1
+    } else if (event.target.drop1.value === "style"){
+      styleValue+=1
+    } else if (event.target.drop1.value === "challenge"){
+      challengeValue+=1
+    } else if (event.target.drop1.value === "replay"){
+      replayValue+=1
+    } else if (event.target.drop1.value === "social"){
+      socialValue+=1
+    } else if (event.target.drop1.value === "innovation"){
+      innovationValue+=1
+    } else if (event.target.drop1.value === "freedom"){
+      freedomValue+=1
     }
-    const drop1Value = event.target.drop1.value;
-    // const drop2Value = event.target.drop2.value;
-    // const drop3Value = event.target.drop3.value;
+
+    if (event.target.drop2.value  === "gameplay") {
+      gameplayValue +=1;
+    }  else if (event.target.drop2.value === "story"){
+      storyValue+=1
+    } else if (event.target.drop2.value === "style"){
+      styleValue+=1
+    } else if (event.target.drop2.value === "challenge"){
+      challengeValue+=1
+    } else if (event.target.drop2.value === "replay"){
+      replayValue+=1
+    } else if (event.target.drop2.value === "social"){
+      socialValue+=1
+    } else if (event.target.drop2.value === "innovation"){
+      innovationValue+=1
+    } else if (event.target.drop2.value === "freedom"){
+      freedomValue+=1
+    }
+
+    if (event.target.drop3.value  === "gameplay") {
+      gameplayValue +=1;
+    }  else if (event.target.drop3.value === "story"){
+      storyValue+=1
+    } else if (event.target.drop3.value === "style"){
+      styleValue+=1
+    } else if (event.target.drop3.value === "challenge"){
+      challengeValue+=1
+    } else if (event.target.drop3.value === "replay"){
+      replayValue+=1
+    } else if (event.target.drop3.value === "social"){
+      socialValue+=1
+    } else if (event.target.drop3.value === "innovation"){
+      innovationValue+=1
+    } else if (event.target.drop3.value === "freedom"){
+      freedomValue+=1
+    }
+    
     let propertiesToUpdate = {
-      // story: game.story + event.target.story.value,
+      story: game.story += storyValue,
       gameplay: game.gameplay += gameplayValue,
-      // style: game.style + event.target.style.value,
-      // challenge: game.challenge + event.target.challenge.value,
-      // replay: game.replay + event.target.replay.value,
-      // social: game.social + event.target.social.value,
-      // innovation: game.innovation + event.target.innovation.value,
-      // freedom: game.freedom + event.target.freedom.value,
+      style: game.style += styleValue,
+      challenge: game.challenge += challengeValue,
+      replay: game.replay += replayValue,
+      social: game.social += socialValue,
+      innovation: game.innovation += innovationValue,
+      freedom: game.freedom += freedomValue,
     };
     return firestore.update(
       {collection: "games", doc: game.id },
@@ -51,26 +106,26 @@ function AddReviewForm(props) {
             <option value="innovation">Innovation</option>
             <option value="freedom">Freedom</option>
           </Form.Control>
-          {/* <Form.Control as='select' name='drop2'>
-            <option name="story">Story</option>
-            <option name="gameplay">Gameplay</option>
-            <option name="style">Style</option>
-            <option name="challenge">Challenge</option>
-            <option name="replay">Replay name</option>
-            <option name="social">Social/Community</option>
-            <option name="innovation">Innovation</option>
-            <option name="freedom">Freedom</option>
+          <Form.Control as='select' name='drop2'>
+            <option value="story" >Story</option>
+            <option value="gameplay">Gameplay</option>
+            <option value="style">Style</option>
+            <option value="challenge">Challenge</option>
+            <option value="replay">Replay value</option>
+            <option value="social">Social/Community</option>
+            <option value="innovation">Innovation</option>
+            <option value="freedom">Freedom</option>
           </Form.Control>
           <Form.Control as='select' name='drop3'>
-            <option name="story">Story</option>
-            <option name="gameplay">Gameplay</option>
-            <option name="style">Style</option>
-            <option name="challenge">Challenge</option>
-            <option name="replay">Replay name</option>
-            <option name="social">Social/Community</option>
-            <option name="innovation">Innovation</option>
-            <option name="freedom">Freedom</option>
-          </Form.Control> */}
+            <option value="story" >Story</option>
+            <option value="gameplay">Gameplay</option>
+            <option value="style">Style</option>
+            <option value="challenge">Challenge</option>
+            <option value="replay">Replay value</option>
+            <option value="social">Social/Community</option>
+            <option value="innovation">Innovation</option>
+            <option value="freedom">Freedom</option>
+          </Form.Control>
         <Button type='submit'>Submit</Button>
         </Form.Group>
       
